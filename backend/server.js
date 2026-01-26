7075532101047f0e387541ac058233c47f0d5b53
22f3001059 <67051548+Gaurangn127@users.noreply.github.com>
2026-01-27 01:34:08 +0530

    setup router

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+// express app 
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((request, res, next) => {
+    console.log(request.path, request.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// listen for requests 
+app.listen(process.env.PORT, () => {
+    console.log("listening on port 4000")
+})
