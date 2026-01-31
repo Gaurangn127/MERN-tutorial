d3810d416be2ec023b71d0de7e8ba3db4978645f
22f3001059 <67051548+Gaurangn127@users.noreply.github.com>
2026-02-01 00:43:50 +0530

    setup login/signup

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
-
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 
 // express app 
@@ -20,6 +20,7 @@ app.use((request, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connnect db
 mongoose.connect(process.env.MONGO_URI)
